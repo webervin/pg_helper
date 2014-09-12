e5fe84b68f1b7c5b6183a7000174af46120fc95b
Ervin <webervin@gmail.com>
2014-09-13 00:21:55 +0300

    converted to rspec 3+

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,10 @@ require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
 require File.expand_path('../../lib/pg_helper', __FILE__)
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
